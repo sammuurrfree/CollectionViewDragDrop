626537685cc4666a6442d8ac44dc85cbfa5346c4
Yes <dragonn050@mail.ru>
2021-11-24 14:16:15 +0300

    Initial Commit

---
+++ CollectionViewDragDrop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewDragDrop
+//
+//  Created by Преподаватель on 24.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
